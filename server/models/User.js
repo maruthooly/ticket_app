b223202c1c6f7c7103584bee332328374ed3bf12
marut <maruthooly@gmail.com>
2025-09-22 02:00:58 +0700

    a

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
     _id:{type: String, required: true},
-    naem:{type:String , required:true},
+    name:{type: String , required:true},
     email:{type: String, required:true},
     image:{type: String, required:true},
 })
