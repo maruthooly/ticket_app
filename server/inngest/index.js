8d0f083802db34553c2294f51f99c3bf45277127
marut <maruthooly@gmail.com>
2025-09-22 03:09:37 +0700

    first

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -17,7 +17,7 @@ const syncUserCreation = inngest.createFunction(
 
         const userData ={
             _id: id,
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
         }
