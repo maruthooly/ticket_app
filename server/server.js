178b0d6523ed7063f3282e7c555dcd4737237a26
marut <maruthooly@gmail.com>
2025-09-22 01:53:44 +0700

    update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import db from './configs/db.js'
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -18,8 +19,10 @@ app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
 
-//API Route
+//API Routes
 app.get('/', (req , res) => res.send('server is live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/show',showRouter)
+
 
 app.listen(port , () => console.log(`Server listening at http://localhost:${port}`))
